bb9e393793c0f372ecc3b340255282faa8cd1c6e
samragi99 <143216421+samragi99@users.noreply.github.com>
2023-09-21 02:18:38 -0500

    create header by soma

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Header from "./header";
+import Footer from "./footer";
 import styles from "./global.module.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Header />
+    <Footer />
   </React.StrictMode>
 );
